f2dee0622b4dd55c4661643288e9fc3fbb18821f
Chris Frosztega <chris.frosztega@gmail.com>
2026-06-23 21:12:44 -0400

    fix: allow Authorization header in CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
     res.setHeader('Vary', 'Origin');
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key,Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
